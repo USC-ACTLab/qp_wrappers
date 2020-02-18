79e2401e3f79aebb840fcb23c4eab20ae33709c6
Vikraman Sathiyanarayanan <vikramansathiyanarayanan@vikramans-mbp.home>
2020-02-18 12:16:07 -0800

    gurobi completed

--- build/third_party/eigen/unsupported/Eigen/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vikramansathiyanarayanan/Desktop/qp_wrappers/third_party/eigen/unsupported/Eigen
-# Build directory: /Users/vikramansathiyanarayanan/Desktop/qp_wrappers/build/third_party/eigen/unsupported/Eigen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("CXX11")
--- build/third_party/eigen/unsupported/doc/snippets/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vikramansathiyanarayanan/Desktop/qp_wrappers/third_party/eigen/unsupported/doc/snippets
-# Build directory: /Users/vikramansathiyanarayanan/Desktop/qp_wrappers/build/third_party/eigen/unsupported/doc/snippets
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
