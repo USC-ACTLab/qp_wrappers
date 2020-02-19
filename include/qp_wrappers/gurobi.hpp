c17908cc2ddad89d060eea0f68f3c2fb859fdfc7
Vikraman Sathiya Narayanan <33651274+vikikkdi@users.noreply.github.com>
2020-02-18 18:36:06 -0800

    Update gurobi.hpp

--- include/qp_wrappers/gurobi.hpp
+++ include/qp_wrappers/gurobi.hpp
@@ -11,77 +11,46 @@ namespace qp_wrappers{
 	namespace gurobi {
 		class solver{
 			public:
-				solver(){
 
+				GRBEnv* env = 0;
+				
+				solver(){
+					env = new GRBEnv();
+					env->set("OutputFlag", "0");
 				}
 
 
 				template<typename T>
 				return_type solve(const qp<T>& problem, typename qp<T>::Vector& primal_solution) {
-
-					GRBEnv env = GRBEnv();
-					env.set("OutputFlag", "0");
-					env.start();
-
+					
+					env->start();
 					// Create an empty model
-					GRBModel model = GRBModel(env);
-
-					qp<c_float>::Matrix A = problem.A;
-					qp<c_float>::Matrix Q = problem.Q;
+					GRBModel model = GRBModel(*env);
 
 					int n = problem.variable_count, m = problem.A.rows();
 					
-					qp<c_float>::Vector lb = problem.lb;
-					qp<c_float>::Vector ub = problem.ub;
-					qp<c_float>::Vector lbx = problem.lbx;
-					qp<c_float>::Vector ubx = problem.ubx;
-					qp<c_float>::Vector c = problem.c;
-
-					double * lB = new double[n];
-					for(int i=0;i<n;i++)	lB[i] = lbx(i);
-					double * uB = new double[n];
-					for(int i=0;i<n;i++)	uB[i] = ubx(i);
-
-					double * q = new double[n*n];
-					int index = 0;
-					for(int i=0;i<n;i++){
-						for(int j=0;j<n;j++){
-							q[index] = Q(i, j);
-							index++;
-						}
-					}
-
-					double * a = new double[m*n];
-					index = 0;
-					for(int i=0;i<m;i++){
-						for(int j=0;j<n;j++){
-							a[index] = A(i, j);
-							index++;
-						}
-					}
-
-					GRBVar *x = model.addVars(lB, uB, NULL, NULL, NULL, n);
+					GRBVar *x = model.addVars(problem.lbx.data(), problem.ubx.data(), NULL, NULL, NULL, n);
 
 					for(int i=0;i<m;i++){
 						GRBLinExpr lhs = 0;
 						for(int j=0;j<n;j++){
-							lhs += a[i*n+j]*x[j];
+							lhs += problem.A(i, j)*x[j];
 						}
-						model.addConstr(lhs <= ub(i));
-						model.addConstr(lhs >= lb(i));
+						model.addConstr(lhs <= problem.ub(i));
+						model.addConstr(lhs >= problem.lb(i));
 					}
 
 
 					GRBQuadExpr obj1 = 0;
 
 					for(int j=0;j<n;j++){
-						obj1 += c(j)*x[j];
+						obj1 += problem.c(j)*x[j];
 					}
 
 					GRBQuadExpr obj2 = 0;
 					for(int i=0;i<n;i++){
 						for(int j=0;j<n;j++){
-							obj2 += q[i*n+j]*x[i]*x[j];
+							obj2 += problem.Q(i, j)*x[i]*x[j];
 						}
 					}
 					obj2 *= 0.5;
@@ -98,19 +67,12 @@ namespace qp_wrappers{
 							primal_solution(i) = x[i].get(GRB_DoubleAttr_X);
 						
 						delete[] x;
-						delete[] a;
-						delete[] q;
-						delete[] lB;
-						delete[] uB;
+						
 						return success;
 					}
 
 					delete[] x;
-					delete[] a;
-					delete[] q;
-					delete[] lB;
-					delete[] uB;
-
+					
 					return infeasible;
 
 				}
