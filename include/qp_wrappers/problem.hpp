4eabc79067d8168b48b30191fc3265f6531ec061
Baskin Senbaslar <basbursen@gmail.com>
2020-03-04 20:32:25 -0800

    Problem class updated

--- include/qp_wrappers/problem.hpp
+++ include/qp_wrappers/problem.hpp
@@ -2,24 +2,39 @@
 #define QPWRAPPERS_PROBLEM_HPP
 
 #include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
 #include <limits>
 #include <iostream>
 
 
-namespace qp_wrappers {
+namespace QPWrappers {
 
+/**
+    Defines a quadratic program in the form 
+         minimize 1/2 x^T Q x + c^T x
+         subject to
+           lb <= Ax <= ub
+           lbx <= x <= ubx
+    where Q is a symmetric matrix.
+*/
 template<typename T>
-class qp {
+class Problem {
     public:
         using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
         using Vector = Eigen::Matrix<T, Eigen::Dynamic, 1>;
         using RowVector = Eigen::Matrix<T, 1, Eigen::Dynamic>;
         using Index = Eigen::Index;
 
-        qp(Index N, Index M = 0):
+        /*
+            Construct a QP with N variables with M constraints.
+            M can be omitted, in which case required constraints must
+            be added one by one.
+        */
+        Problem(Index N, Index M = 0):
                 A_mtr(M, N),
                 lb_mtr(M),
-                ub_mtr(M) {
+                ub_mtr(M),
+                is_Q_psd_calculated(false) {
             Q_mtr.setConstant(N, N, 0);
             c_mtr.setConstant(N, 0);
             lbx_mtr.setConstant(N, std::numeric_limits<T>::lowest());
@@ -42,14 +57,29 @@ class qp {
             return A_mtr.rows();
         }
 
+        /*
+            Sets the constraint with index constraint_idx.
+            After the operation, low <= coeff * x <= up is enforced by the
+            row with index constraint_idx of A, lb, and ub.
+        */
         void set_constraint(Index constraint_idx, const RowVector& coeff, T low, T up) {
-            if(coeff.cols() != A_mtr.cols()) {
+            if(coeff.cols() != num_vars()) {
                 throw std::domain_error(
                     std::string("Problem has ")
-                    + std::to_string(A_mtr.cols())
+                    + std::to_string(num_vars())
                     + std::string(" variables, but the provided row vector for constraint has ")
                     + std::to_string(coeff.cols())
                     + std::string(" columns.")
+                );
+            }
+
+            if(constraint_idx >= num_constraints()) {
+                throw std::domain_error(
+                    std::string("constraint index out of range. ")
+                    + std::string("constraint_idx: ") 
+                    + std::to_string(constraint_idx)
+                    + std::string(", num constraints: ") 
+                    + std::to_string(num_constraints())
                 );
             }
             
@@ -58,31 +88,52 @@ class qp {
             lb_mtr(constraint_idx) = low;
         }
 
+        /*
+            Adds a new constraint which enforces
+                low <= coeff * x <= up
+        */
         void add_constraint(const RowVector& coeff, T low, T up) {
-            if(coeff.cols() != A_mtr.cols()) {
+            if(coeff.cols() != num_vars()) {
                 throw std::domain_error (
                     std::string("Problem has ")
-                    + std::to_string(A_mtr.cols())
+                    + std::to_string(num_vars())
                     + std::string(" variables, but the provided row vector for constraint has ")
                     + std::to_string(coeff.cols())
                     + std::string(" columns.")
                 );
             }
 
-            A_mtr.conservativeResize(A_mtr.rows() + 1, Eigen::NoChange_t());
-            ub_mtr.conservativeResize(ub_mtr.rows() + 1, Eigen::NoChange_t());
-            lb_mtr.conservativeResize(lb_mtr.rows() + 1, Eigen::NoChange_t());
+            A_mtr.conservativeResize(A_mtr.rows() + 1, no_change);
+            ub_mtr.conservativeResize(ub_mtr.rows() + 1, no_change);
+            lb_mtr.conservativeResize(lb_mtr.rows() + 1, no_change);
 
             A_mtr.row(A_mtr.rows() - 1) = coeff;
             ub_mtr(ub_mtr.rows() - 1) = up;
             lb_mtr(lb_mtr.rows() - 1) = low;
         }
 
+        /*
+            Sets the limits of variable with index var_idx. Enforces
+                low <= x[var_idx] <= up
+        */
         void set_var_limits(Index var_idx, T low, T up) {
+            if(var_idx >= num_vars()) {
+                throw std::domain_error(
+                    std::string("Problem has ")
+                    + std::to_string(num_vars())
+                    + std::string(" variables. set_var_limits is used for variable with index ")
+                    + std::to_string(var_idx)
+                );
+            }
+
             lbx_mtr(var_idx) = low;
             ubx_mtr(var_idx) = up;
         }
 
+        /*
+            Adds given matrix to the Q of the problem.
+            If given matrix is not symmetric, makes it symmetric first.
+        */
         void add_Q(const Matrix& Q) {
             if(Q.rows() != Q_mtr.rows() || Q.cols() != Q_mtr.cols()) {
                 throw std::domain_error(
@@ -97,9 +148,46 @@ class qp {
                             + std::string(" was provided to add to it.")
                             );
             }
+
             Q_mtr += Q;
+
+            // Ensure it is symmetric
+            for(Index i = 0; i < Q_mtr.rows(); i++) {
+                for (Index j = i+1; j < Q_mtr.cols(); j++) {
+                    Q_mtr(i, j) = Q_mtr(j, i) = (Q_mtr(i, j) / 2) + (Q_mtr(j, i) / 2);
+                }
+            }
+
+            // set psd calculated flag to false
+            is_Q_psd_calculated = false;
         }
 
+        /*
+            return if Q is a PSD matrix
+        */
+        bool is_Q_psd() {
+            // compute of Q is psd on the fly, and store the result into m_is_Q_psd.
+            if(!is_Q_psd_calculated) {
+                Eigen::EigenSolver<Matrix> eigen_solver(Q_mtr, false);
+                const auto& eigen_values = eigen_solver.eigenvalues();
+
+                m_is_Q_psd = true;
+                for(auto eigen_value: eigen_values) {
+                    if(eigen_value < 0) {
+                        m_is_Q_psd = false;
+                        break;
+                    }
+                }
+
+                is_Q_psd_calculated = true;
+            }
+
+            return m_is_Q_psd;
+        }
+
+        /*
+            Adds given vector to c of the problem.
+        */
         void add_c(const Vector& c) {
             if(c.rows() != c_mtr.rows()) {
                 throw std::domain_error(
@@ -112,6 +200,7 @@ class qp {
             }
             c_mtr += c;
         }
+
 
         const Matrix& Q() const {
             return Q_mtr;
@@ -141,10 +230,12 @@ class qp {
             return ubx_mtr;
         }
 
-
+        /*
+            Cast problem into type S
+        */
         template<class S>
-        qp<S> cast() const {
-            qp<S> new_problem;
+        Problem<S> cast() const {
+            Problem<S> new_problem;
             new_problem.Q_mtr = Q_mtr.template cast<S>();
             new_problem.A_mtr = A_mtr.template cast<S>();
             new_problem.c_mtr = c_mtr.template cast<S>();
@@ -152,79 +243,92 @@ class qp {
             new_problem.ub_mtr = ub_mtr.template cast<S>();
             new_problem.lbx_mtr = lbx_mtr.template cast<S>();
             new_problem.ubx_mtr = ubx_mtr.template cast<S>();
+            new_problem.m_is_Q_psd = m_is_Q_psd;
+            new_problem.is_Q_psd_calculated = is_Q_psd_calculated;
+
             return new_problem;
         }
 
+        /*
+            Simply check if constraints are consistent.
+            Basically checks if any lower bound is more than any upper bound.
+        */
         bool is_consistent() const {
-            bool consistent = true;
             for(int i = 0; i < num_vars(); i++) {
                 if(ubx_mtr(i) < lbx_mtr(i)) {
-                    consistent = false;
-                    std::cout << "bound on variable " << i << " is inconsistent. lbx " << lbx_mtr(i) << " ubx " << ubx_mtr(i) << std::endl;
+                    return false;
                 }
             }
 
             for(int i = 0; i < num_constraints(); i++) {
                 if(ub_mtr(i) < lb_mtr(i)) {
-                    consistent = false;
-                    std::cout << "bound on constraint " << i << " is inconsistent. lb " << lb_mtr(i) << " ub " << ub_mtr(i) << std::endl; 
+                    return false;
                 }
             }
-            return consistent;
+
+            return true;
         }
 
-        bool verify(const Vector& solution) const {
-            std::cout 
-                    << "obj: "  
-                    << 0.5 * solution.transpose() * Q_mtr * solution + solution.transpose() * c_mtr 
-                    << std::endl;
+        /*
+            Verifies that solution is consistent with the constraints,
+            that is it doesn't violate any constraint under the given tolarance.
+            Violation by tolerance amount is accepted
+        */
+        bool verify(const Vector& solution, T tolerance = 0) const {
+
+            if(solution.rows() != num_vars()) {
+                throw std::domain_error(
+                    std::string("Problem has ")
+                    + std::to_string(num_vars())
+                    + std::string(" variables, but given solution has ")
+                    + std::to_string(solution.rows())
+                    + std::string(" rows.")
+                );
+            }
 
             Vector constraint_mtr = A_mtr * solution;
 
-            bool constraints_satisfied = true;
             for(int i = 0; i < num_constraints(); i++) {
-                constraints_satisfied = constraints_satisfied && lb_mtr(i) <= constraint_mtr(i) && constraint_mtr(i) <= ub_mtr(i);
-                if(!(lb_mtr(i) <= constraint_mtr(i) && constraint_mtr(i) <= ub_mtr(i)))
-                    std::cout << "constraint " << i << " violated: " 
-                              << ": lb " << lb_mtr(i) << "<=" << constraint_mtr(i) << " " << (lb_mtr(i) <= constraint_mtr(i)) 
-                              << " ub " << constraint_mtr(i) <<  "<=" << ub_mtr(i) << " " << (constraint_mtr(i) <= ub_mtr(i)) << std::endl;
+                if(lb_mtr(i) - tolerance > constraint_mtr(i)) {
+                    return false;
+                }
+
+                if(constraint_mtr(i) > ub_mtr(i) + tolerance) {
+                    return false;
+                }
             }
 
-            bool bounds_satisfied = true;
             for(int i = 0; i < num_vars(); i++) {
-                bounds_satisfied = bounds_satisfied && lbx_mtr(i) <= solution(i) && solution(i) <= ubx_mtr(i); 
-                if(!(lbx_mtr(i) <= solution(i) && solution(i) <= ubx_mtr(i)))
-                    std::cout << "bounds for variable " << i << " violated: " 
-                              << ": lb " << lbx_mtr(i) <<  "<=" << solution(i) << " " << (lbx_mtr(i) <= solution(i)) 
-                              << " ub " << solution(i) << "<=" << ubx_mtr(i) << " " << (solution(i) <= ubx_mtr(i)) << std::endl; 
+                if(lbx_mtr(i) - tolerance > solution(i)) {
+                    return false;
+                }
+
+                if(solution(i) > ubx_mtr(i) + tolerance) {
+                    return false; 
+                }
             }
             
-            return constraints_satisfied && bounds_satisfied;
+            return true;
         }
 
         template<typename S>
-        friend std::ostream& operator<<(std::ostream& os, const qp<S>& problem);
+        friend std::ostream& operator<<(std::ostream& os, const Problem<S>& problem);
         template<typename S>
-        friend std::istream& operator>>(std::istream& is, qp<S>& problem);
+        friend std::istream& operator>>(std::istream& is, Problem<S>& problem);
 
     private:
-
-        /*
-        * minimize 1/2 x^T Q x + c^T x
-        * subject to
-        *   lb <= Ax <= ub
-        *   lbx <= x <= ubx
-        */
-
-
-        qp() {} // for internal operations (like casting)
+        Problem() {} // for internal operations (like casting)
 
         Matrix Q_mtr, A_mtr;
         Vector c_mtr, lb_mtr, ub_mtr, lbx_mtr, ubx_mtr;
+        bool m_is_Q_psd, is_Q_psd_calculated;
+
+        static constexpr Eigen::NoChange_t no_change();
+
 };
 
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const qp<T>& problem) {
+std::ostream& operator<<(std::ostream& os, const Problem<T>& problem) {
     auto before_precision = os.precision();
     
     os.precision(std::numeric_limits<T>::max_digits10);
@@ -279,7 +383,7 @@ std::ostream& operator<<(std::ostream& os, const qp<T>& problem) {
 }
 
 template<typename T>
-std::istream& operator>>(std::istream& is, qp<T>& problem) {
+std::istream& operator>>(std::istream& is, Problem<T>& problem) {
     int n,m;
     is >> n >> m;
     // problem.variable_count = n;
