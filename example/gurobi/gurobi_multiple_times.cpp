0406578f3781f0def58a249e9999a93473af9c0f
Baskin Senbaslar <basbursen@gmail.com>
2020-03-10 18:01:30 -0700

    gurobi engine

--- example/gurobi/gurobi_multiple_times.cpp
+++ example/gurobi/gurobi_multiple_times.cpp
@@ -1,4 +1,4 @@
-#include <qp_wrappers/cplex.hpp>
+#include <qp_wrappers/gurobi.hpp>
 #include <qp_wrappers/problem.hpp>
 
 int main() {
@@ -6,17 +6,17 @@ int main() {
     QPWrappers::Problem<double> problem(0);
     std::cin >> problem;
 
-    QPWrappers::CPLEX::Engine<double> cplexEngine;
-    cplexEngine.setFeasibilityTolerance(1e-8);
-    cplexEngine.setPSDCheckEigenvalueTolerance(1e-8);
+    QPWrappers::GUROBI::Engine<double> gurobiEngine;
+    gurobiEngine.setFeasibilityTolerance(1e-8);
+    gurobiEngine.setPSDCheckEigenvalueTolerance(1e-8);
     
     
     QPWrappers::Problem<double>::Vector result;
-    cplexEngine.init(problem, result);
+    gurobiEngine.init(problem, result);
     int cnt = 10000000;
     for(int i = 0; i < cnt; i++) {
         std::cout << i << "/" << cnt << std::endl;
-        cplexEngine.next(problem, result);
+        gurobiEngine.next(problem, result);
 
         std::cout << "verification: " << problem.verify(result, 1e-10) << std::endl;
         std::cout << "objective value: " << problem.objective(result) << std::endl;
--- include/qp_wrappers/gurobi.hpp
+++ include/qp_wrappers/gurobi.hpp
@@ -6,75 +6,138 @@
 #include <gurobi_c++.h>
 #include <string>
 
-namespace qp_wrappers{
+namespace QPWrappers {
+    namespace GUROBI {
+        
+        /*
+            A QP engine that solves consecutive QP instances where the result of the previous
+            instance used as an initial guess to the next one unless an initial guess
+            is provided.
+        */
+        template<typename T>
+        class Engine {
+            public:
+                Engine() {
+                    env.set(GRB_IntParam_OutputFlag, 0);
+                    env.set(GRB_DoubleParam_FeasibilityTol, 1e-6);
+                    env.set(GRB_DoubleParam_PSDTol, 0);
+                    env.set(GRB_IntParam_Method, 0);
+                    env.start();
+                }
 
-	namespace gurobi {
-		class solver{
-			public:
+                Engine(const Engine& rhs) = delete;
+                Engine& operator=(const Engine& rhs) = delete;
 
-				GRBEnv env;
-				
-				solver() {
-					env.set("OutputFlag", "0");
-					env.start();
-				}
+                Engine(Engine&& rhs) = delete;
+                Engine& operator=(Engine&& rhs) = delete;
 
+                ~Engine() {
+                }
 
-				template<typename T>
-				return_type solve(const qp<T>& problem, typename qp<T>::Vector& primal_solution) {
-					// Create an empty model
-					GRBModel model = GRBModel(env);
+                /*
+                    By how much are the constraints allowed to be violated?
+                */
+                void setFeasibilityTolerance(T tolerance) {
+                    env.set(GRB_DoubleParam_FeasibilityTol, tolerance);
+                }
 
-					GRBVar *x = model.addVars(problem.lbx().data(), problem.ubx().data(), NULL, NULL, NULL, problem.num_vars());
+                /*
+                    By how much are the eigenvalues of the Q matrix are allowed be below 0 during PSD check?
+                */
+                void setPSDCheckEigenvalueTolerance(T tolerance) {
+                    env.set(GRB_DoubleParam_PSDTol, tolerance);
+                }
 
-					for(int i = 0; i < problem.num_constraints(); i++) {
-						GRBLinExpr lhs = 0;
-						for(int j = 0; j < problem.num_vars(); j++) {
-							lhs += problem.A()(i, j) * x[j];
-						}
+                /*
+                    Solve the first intance of the set of problems.
+                    Load solution result to result.
+                */
+                OptReturnType init(const Problem<T>& problem, typename Problem<T>::Vector& result) {
+                    GRBModel model{env};
+                    GRBVar* vars = model.addVars(problem.lbx().data(), problem.ubx().data(), NULL, NULL, NULL, problem.num_vars());
 
-						model.addConstr(lhs <= problem.ub()(i));
-						model.addConstr(lhs >= problem.lb()(i));
-					}
+                    for(typename Problem<T>::Index i = 0; i < problem.num_constraints(); i++) {
+                        GRBLinExpr expr;
+                        for(typename Problem<T>::Index j = 0; j < problem.num_vars(); j++) {
+                            expr += problem.A()(i, j) * vars[j];
+                        }
 
+                        model.addConstr(expr, GRB_LESS_EQUAL, problem.ub()(i));
+                        model.addConstr(expr, GRB_GREATER_EQUAL, problem.lb()(i));
+                    }
 
-					GRBQuadExpr obj1 = 0;
-					for(int j = 0; j < problem.num_vars(); j++) {
-						obj1 += problem.c()(j) * x[j];
-					}
+                    GRBQuadExpr obj_quad{0};
+                    for(typename Problem<T>::Index i = 0; i < problem.num_vars(); i++) {
+                        for(typename Problem<T>::Index j = 0; j < problem.num_vars(); j++) {
+                            obj_quad += problem.Q()(i, j) * vars[i] * vars[j];
+                        }
+                    }
+                    obj_quad *= 0.5;
 
-					GRBQuadExpr obj2 = 0;
-					for(int i = 0; i < problem.num_vars(); i++) {
-						for(int j = 0; j < problem.num_vars(); j++) {
-							obj2 += problem.Q()(i, j) * x[i] * x[j];
-						}
-					}
-					obj2 *= 0.5;
+                    GRBLinExpr obj_lin{0};
+                    for(typename Problem<T>::Index i = 0; i < problem.num_vars(); i++) {
+                        obj_lin += problem.c()(i) * vars[i];
+                    }
 
-					GRBQuadExpr obj = obj1 + obj2;
+                    model.setObjective(obj_quad + obj_lin);
+                    model.optimize();
 
-					model.setObjective(obj);
-					model.optimize();
+                    auto status = model.get(GRB_IntAttr_Status);
 
-					if (model.get(GRB_IntAttr_Status) == GRB_OPTIMAL) {
+                    if(status == GRB_OPTIMAL) {
+                        loadResult(vars, problem.num_vars(), result);
+                        delete[] vars;
+                        return OptReturnType::Optimal;
+                    } else if(status == GRB_INFEASIBLE) {
+                        delete[] vars;
+                        return OptReturnType::Infeasible;
+                    } else if (status == GRB_INF_OR_UNBD) {
+                        delete[] vars;
+                        return OptReturnType::InfeasibleOrUnbounded;
+                    } else if(status == GRB_UNBOUNDED) {
+                        delete[] vars;
+                        loadResult(vars, problem.num_vars(), result);
+                        return OptReturnType::Unbounded;
+                    } else if (status == GRB_NUMERIC) {
+                        delete[] vars;
+                        return OptReturnType::Error;
+                    } else if (status == GRB_SUBOPTIMAL) {
+                        delete[] vars;
+                        loadResult(vars, problem.num_vars(), result);
+                        return OptReturnType::Feasible;
+                    }
 
-						primal_solution.resize(problem.num_vars());
-						
-						for (int i = 0; i < problem.num_vars(); i++)
-							primal_solution(i) = x[i].get(GRB_DoubleAttr_X);
-						
-						delete[] x;
-						
-						return success;
-					}
+                    delete[] vars;
+                    return OptReturnType::Unknown;
+                }
 
-					delete[] x;
-					
-					return infeasible;
+                /*
+                    Solve the next problem of the set of problems.
+                */
+                OptReturnType next(const Problem<T>& problem, typename Problem<T>::Vector& result) {
+                    return init(problem, result);
+                }
 
-				}
-		};
-	}
+                /*
+                    Solve the next problem of the set of problems with the given initial guess.
+                    Since there is no concept of feeding initial guess in GUROBI, we simply call init.
+                */
+                OptReturnType next(const Problem<T>& problem, typename Problem<T>::Vector& result, const typename Problem<T>::Vector& initial_guess) {
+                    return init(problem, result);
+                }
+
+            private:
+                GRBEnv env;
+
+                void loadResult(GRBVar* vars, typename Problem<T>::Index var_count, typename Problem<T>::Vector& result) {
+                    result.resize(var_count);
+
+                    for(typename Problem<T>::Index i = 0; i < var_count; i++) {
+                        result(i) = vars[i].get(GRB_DoubleAttr_X);
+                    }
+                }
+        };
+    }
 }
 
 #endif
