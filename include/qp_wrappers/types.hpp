7b42677a84badb855ae17f9e19eb83cfe46005b9
Baskin Senbaslar <basbursen@gmail.com>
2020-06-09 02:40:45 -0700

    operator<< for optreturntype added

--- include/qp_wrappers/types.hpp
+++ include/qp_wrappers/types.hpp
@@ -1,6 +1,8 @@
 #ifndef QPWRAPPERS_TYPES_HPP
 #define QPWRAPPERS_TYPES_HPP
 
+#include <iostream>
+
 namespace QPWrappers {
 
 enum class OptReturnType {
@@ -13,6 +15,36 @@ enum class OptReturnType {
     InfeasibleOrUnbounded
 };
 
+}
+
+std::ostream& operator<<(std::ostream& os,
+        QPWrappers::OptReturnType ret_type) {
+    using namespace QPWrappers;
+    switch(ret_type) {
+        case OptReturnType::Optimal:
+            os << "Optimal";
+            break;
+        case OptReturnType::Feasible:
+            os << "Feasible";
+            break;
+        case OptReturnType::Unbounded:
+            os << "Unbounded";
+            break;
+        case OptReturnType::Infeasible:
+            os << "Infeasible";
+            break;
+        case OptReturnType::Error:
+            os << "Error";
+            break;
+        case OptReturnType::Unknown:
+            os << "Unknown";
+            break;
+        case OptReturnType::InfeasibleOrUnbounded:
+            os << "InfeasibleOrUnbounded";
+            break;
+    }
+
+    return os;
 }
 
 #endif
